d5cfdc11830e9737b8ed446f5326ea00b4d21049
Michael <gblkk@yandex.ru>
2016-02-12 00:47:26 +0300

    2nd try

---
+++ LazyFactory/src/main/java/mit/spbau/ru/LazyFactory.java
@@ -0,0 +1,7 @@
+package mit.spbau.ru;
+
+/**
+ * Created by michael on 12.02.16.
+ */
+public class LazyFactory {
+}
---
+++ LazyFactory/src/test/java/mit/spbau/ru/LazyFactoryTest.java
@@ -0,0 +1,12 @@
+package mit.spbau.ru;
+
+import org.junit.Test;
+
+/**
+ * Created by michael on 12.02.16.
+ */
+public class LazyFactoryTest {
+    @Test
+    public void easyTest() {
+    }
+}
