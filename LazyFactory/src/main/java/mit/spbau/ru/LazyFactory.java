53f57372296dd309469b0c6839f3eee0404886ae
Michael <gblkk@yandex.ru>
2016-02-13 02:51:36 +0300

    finished

--- LazyFactory/src/main/java/mit/spbau/ru/LazyFactory.java
+++ LazyFactory/src/main/java/mit/spbau/ru/LazyFactory.java
@@ -1,7 +1,66 @@
 package mit.spbau.ru;
 
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+import java.util.function.Supplier;
+
 /**
  * Created by michael on 12.02.16.
  */
+
 public class LazyFactory {
+    private final static Object DEFAULD_UTIL_OBJ = new Object();
+
+    public static <T> Lazy<T> createLazy(Supplier<T> supplier) {
+        return new Lazy<T>() {
+            private Object result = LazyFactory.DEFAULD_UTIL_OBJ;
+
+            @Override
+            public T get() {
+                if (result == LazyFactory.DEFAULD_UTIL_OBJ) {
+                    result = supplier.get();
+                }
+                return (T) result;
+            }
+        };
+    }
+
+    public static <T> Lazy<T> createLazyConcurrent(Supplier<T> supplier) {
+        return new Lazy<T>() {
+            private volatile Object result = LazyFactory.DEFAULD_UTIL_OBJ;
+
+            @Override
+            public T get() {
+                if (result == LazyFactory.DEFAULD_UTIL_OBJ) {
+                    synchronized (this) {
+                        if (result == LazyFactory.DEFAULD_UTIL_OBJ) {
+                            result = supplier.get();
+                        }
+                    }
+                }
+                return (T) result;
+            }
+        };
+    }
+
+    public static <T> Lazy<T> createLazyConcurrentAtomic(Supplier<T> supplier) {
+        return new AtomicLazy<>(supplier);
+    }
+
+    private static class AtomicLazy<T> implements Lazy<T> {
+        private Supplier<T> supplier;
+        private volatile Object result = LazyFactory.DEFAULD_UTIL_OBJ;
+
+        public AtomicLazy(Supplier<T> supplier) {
+            this.supplier = supplier;
+        }
+
+        @Override
+        public T get() {
+            AtomicReferenceFieldUpdater<AtomicLazy, Object> updator =
+                    AtomicReferenceFieldUpdater.newUpdater(AtomicLazy.class, Object.class, "result");
+            updator.compareAndSet(this, LazyFactory.DEFAULD_UTIL_OBJ, supplier.get());
+            return (T) result;
+        }
+    }
 }
+
--- LazyFactory/src/test/java/mit/spbau/ru/LazyFactoryTest.java
+++ LazyFactory/src/test/java/mit/spbau/ru/LazyFactoryTest.java
@@ -2,11 +2,96 @@ package mit.spbau.ru;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.function.Supplier;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by michael on 12.02.16.
  */
+
 public class LazyFactoryTest {
+    private static class IncrementSupplier implements Supplier<Integer> {
+        private int counter = 0;
+        private boolean executionAllowed = false;
+
+        public void allowExecuting() {
+            executionAllowed = true;
+        }
+
+        @Override
+        public Integer get() {
+            assertTrue(executionAllowed);
+            counter += 1;
+            return counter;
+        }
+    }
+
+    private void multiThreadTest(Lazy<Integer> lazy) {
+        ArrayList<Thread> threadList = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            threadList.add(new Thread(() -> assertEquals(lazy.get(), (Integer) 1)));
+        }
+        for (int i = 0; i < 100; i++) {
+            threadList.get(i).run();
+        }
+        for (int i = 0; i < 100; i++) {
+            try {
+                threadList.get(i).join();
+            } catch (InterruptedException exc) {
+                exc.printStackTrace();
+                throw new AssertionError();
+            }
+        }
+    }
+
     @Test
-    public void easyTest() {
+    public void createLazyTest() {
+        IncrementSupplier incrementSupplier = new IncrementSupplier();
+        Lazy<Integer> lazy = LazyFactory.createLazy(incrementSupplier);
+        incrementSupplier.allowExecuting();
+        assertEquals(lazy.get(), (Integer) 1);
+        assertEquals(lazy.get(), (Integer) 1);
+        assertEquals(lazy.get(), (Integer) 1);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void createLazyFailTest() {
+        IncrementSupplier incrementSupplier = new IncrementSupplier();
+        Lazy<Integer> lazy = LazyFactory.createLazy(incrementSupplier);
+        lazy.get();
+    }
+
+    @Test
+    public void createLazyConcurrentTest() {
+        IncrementSupplier incrementSupplier = new IncrementSupplier();
+        Lazy<Integer> lazy = LazyFactory.createLazyConcurrent(incrementSupplier);
+        incrementSupplier.allowExecuting();
+        multiThreadTest(lazy);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void createLazyConcurrentTestFail() {
+        IncrementSupplier incrementSupplier = new IncrementSupplier();
+        Lazy<Integer> lazy = LazyFactory.createLazyConcurrent(incrementSupplier);
+        lazy.get();
+    }
+
+    @Test
+    public void createLazyConcurrentAtomicTest() {
+        IncrementSupplier incrementSupplier = new IncrementSupplier();
+        Lazy<Integer> lazy = LazyFactory.createLazyConcurrentAtomic(incrementSupplier);
+        incrementSupplier.allowExecuting();
+        multiThreadTest(lazy);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void createLazyConcurrentAtomicTestFail() {
+        IncrementSupplier incrementSupplier = new IncrementSupplier();
+        Lazy<Integer> lazy = LazyFactory.createLazyConcurrentAtomic(incrementSupplier);
+        lazy.get();
     }
 }
+
+
